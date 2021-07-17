0d3f73882a07e956af378d1e40f060a593f8f916
thiwaK <thiwanka.kaushal.mob@gmail.com>
2021-07-17 22:04:59 +0530

    World covid data updated

--- src/js/script.js
+++ src/js/script.js
@@ -39,6 +39,7 @@ let config = {
   minZoom: 2,
   maxZomm: 20,
   zoomControl: false,
+  'messagebox': true,
 
   zoom: 3,
   lat: 18,
@@ -68,6 +69,51 @@ var cfg = {
   valueField: 'count'
 };
 
+function messegeBoxFunctionality(){
+  L.Control.Messagebox = L.Control.extend({
+    options: {
+        position: 'topright',
+        timeout: 3000
+    },
+  
+    onAdd: function (map) {
+        this._container = L.DomUtil.create('div', 'leaflet-control-messagebox');
+        //L.DomEvent.disableClickPropagation(this._container);
+        return this._container;
+    },
+  
+    show: function (message, timeout) {
+        var elem = this._container;
+        elem.innerHTML = message;
+        elem.style.display = 'block';
+  
+        timeout = timeout || this.options.timeout;
+  
+        if (typeof this.timeoutID == 'number') {
+            clearTimeout(this.timeoutID);
+        }
+        this.timeoutID = setTimeout(function () {
+            elem.style.display = 'none';
+        }, timeout);
+    }
+  });
+  
+  L.Map.mergeOptions({
+    messagebox: false
+  });
+  
+  L.Map.addInitHook(function () {
+    if (this.options.messagebox) {
+        this.messagebox = new L.Control.Messagebox();
+        this.addControl(this.messagebox);
+    }
+  });
+  
+  L.control.messagebox = function (options) {
+    return new L.Control.Messagebox(options);
+  };
+}
+
 function setAdditionalConfig(){
   map.attributionControl.setPrefix(false);
   map.dragging.enable();
@@ -75,6 +121,94 @@ function setAdditionalConfig(){
   map.doubleClickZoom.enable();
   map.scrollWheelZoom.enable();
 }
+
+// ref: http://stackoverflow.com/a/1293163/2343
+// This will parse a delimited string into an array of
+// arrays. The default delimiter is the comma, but this
+// can be overriden in the second argument.
+function CSVToArray( strData, strDelimiter ){
+  // Check to see if the delimiter is defined. If not,
+  // then default to comma.
+  strDelimiter = (strDelimiter || ",");
+
+  // Create a regular expression to parse the CSV values.
+  var objPattern = new RegExp(
+      (
+          // Delimiters.
+          "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+          // Quoted fields.
+          "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+          // Standard fields.
+          "([^\"\\" + strDelimiter + "\\r\\n]*))"
+      ),
+      "gi"
+      );
+
+
+  // Create an array to hold our data. Give the array
+  // a default empty first row.
+  var arrData = [[]];
+
+  // Create an array to hold our individual pattern
+  // matching groups.
+  var arrMatches = null;
+
+
+  // Keep looping over the regular expression matches
+  // until we can no longer find a match.
+  while (arrMatches = objPattern.exec( strData )){
+
+      // Get the delimiter that was found.
+      var strMatchedDelimiter = arrMatches[ 1 ];
+
+      // Check to see if the given delimiter has a length
+      // (is not the start of string) and if it matches
+      // field delimiter. If id does not, then we know
+      // that this delimiter is a row delimiter.
+      if (
+          strMatchedDelimiter.length &&
+          strMatchedDelimiter !== strDelimiter
+          ){
+
+          // Since we have reached a new row of data,
+          // add an empty row to our data array.
+          arrData.push( [] );
+
+      }
+
+      var strMatchedValue;
+
+      // Now that we have our delimiter out of the way,
+      // let's check to see which kind of value we
+      // captured (quoted or unquoted).
+      if (arrMatches[ 2 ]){
+
+          // We found a quoted value. When we capture
+          // this value, unescape any double quotes.
+          strMatchedValue = arrMatches[ 2 ].replace(
+              new RegExp( "\"\"", "g" ),
+              "\""
+              );
+
+      } else {
+
+          // We found a non-quoted value.
+          strMatchedValue = arrMatches[ 3 ];
+
+      }
+
+
+      // Now that we have our value string, let's add
+      // it to the data array.
+      arrData[ arrData.length - 1 ].push( strMatchedValue );
+  }
+
+  // Return the parsed data.
+  return( arrData );
+}
+
 
 function processData(allText) {
 
@@ -136,63 +270,68 @@ function addAdditionalTileLayers(){
   L.control.layers(baseMaps).addTo(map);
 }
 
+let isLoaded = false;
 function loadSLCovid(){
 
-  const Http = new XMLHttpRequest();
-  const url='https://nhss.gov.lk/api/suwapetha/getPatientData/covid';
-  Http.open("POST", url);
-  Http.send();
+  if(isLoaded == flase){
+    
+    const Http = new XMLHttpRequest();
+    const url='https://nhss.gov.lk/api/suwapetha/getPatientData/covid';
+    Http.open("POST", url);
+    Http.send();
 
-  let pationt = [];
+    let pationt = [];
 
-  Http.onreadystatechange = (e) => {
-    // packet by packet
-  };
+    Http.onreadystatechange = (e) => {
+      // packet by packet
+    };
 
-  Http.onloadend = (e) =>{
-    // whole packet
+    Http.onloadend = (e) =>{
+      // whole packet
 
-    const obj = JSON.parse(Http.responseText);
-    console.log(obj.http_status);
+      const obj = JSON.parse(Http.responseText);
+      console.log(obj.http_status);
 
-    for (let item in obj.patients) {
-      // console.log(obj.patients[item].Date_of_report_received);
-      // console.log(obj.patients[item].lat);
-      // console.log(obj.patients[item].lng);
-      if(obj.patients[item].lat >= 0){
-        if(obj.patients[item].lng >= 0){
-          pationt.push({"lat":parseFloat(obj.patients[item].lat) , "lng":parseFloat(obj.patients[item].lng), count: 1}); 
+      for (let item in obj.patients) {
+        // console.log(obj.patients[item].Date_of_report_received);
+        // console.log(obj.patients[item].lat);
+        // console.log(obj.patients[item].lng);
+        if(obj.patients[item].lat >= 0){
+          if(obj.patients[item].lng >= 0){
+            pationt.push({"lat":parseFloat(obj.patients[item].lat) , "lng":parseFloat(obj.patients[item].lng), count: 1}); 
 
-          var circleCenter = [obj.patients[item].lat, obj.patients[item].lng];
-          var circleOptions = {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: .4
+            var circleCenter = [obj.patients[item].lat, obj.patients[item].lng];
+            var circleOptions = {
+              color: 'red',
+              fillColor: '#f03',
+              fillOpacity: .4
+            }
+            var circle = L.circle(circleCenter, 50, circleOptions);
+            circle.bindPopup("<b>Reported on </b>" + obj.patients[item].Date_of_report_received + 
+            "<br/><b>Latitiude </b>" + obj.patients[item].lat + 
+            "<br/><b>Longitiude </b>" + obj.patients[item].lng);
+            circle.addTo(map);
+
+
           }
-          var circle = L.circle(circleCenter, 50, circleOptions);
-          circle.bindPopup("<b>Reported on </b>" + obj.patients[item].Date_of_report_received + 
-          "<br/><b>Latitiude </b>" + obj.patients[item].lat + 
-          "<br/><b>Longitiude </b>" + obj.patients[item].lng);
-          circle.addTo(map);
-
-
         }
+
+        
       }
 
+      var testData = {
+        "data":pationt,
+      };
       
-    }
-
-    var testData = {
-      "data":pationt,
+      var heatmapLayer = new HeatmapOverlay(cfg);
+      heatmapLayer.setData(testData);
+      //map.addLayer(heatmapLayer);
+      isLoaded = true;
     };
-    
-    var heatmapLayer = new HeatmapOverlay(cfg);
-    heatmapLayer.setData(testData);
-    //map.addLayer(heatmapLayer);
-  };
+  }
 }
 
-function loadCVS(){
+function loadWorldData(){
 
   let Http_3 = new XMLHttpRequest();
   let url_3='/src/countries.csv';
@@ -217,6 +356,96 @@ function loadCVS(){
   
 }
 
+function worldDataSource2(){
+  // https://corona.lmao.ninja/v2/countries?yesterday&sort
+
+  var today = new Date();
+  var yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+
+  var dd = String(yesterday.getDate()).padStart(2, '0');
+  var mm = String(yesterday.getMonth() + 1).padStart(2, '0');
+  var yyyy = yesterday.getFullYear();
+
+  let _date_ = mm + '-' + dd + '-' + yyyy;
+
+    
+  const Http = new XMLHttpRequest();
+  const url='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+_date_+'.csv';
+  Http.open("GET", url);
+  Http.send();
+
+  var dataBundle = {
+    countryName:[],
+    lastUpdate:[],
+    lat:[],
+    lng:[],
+    Confirmed:[],
+    Deaths:[],
+    Recovered:[],
+    Active:[]
+  };
+  
+  Http.onloadend = (e) =>{
+      
+    var allText = Http.responseText;
+
+    var allTextLines = CSVToArray(allText)
+    var count = 1;
+
+    while (allTextLines.length != count){
+
+      let rowArray = allTextLines[count];
+      // FIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key,Incident_Rate,Case_Fatality_Ratio
+
+      if(rowArray[2] == "" || rowArray[2] != ""){  // skip privince_state filter      
+        if (rowArray[5] != undefined || rowArray[6] != undefined){
+          
+          dataBundle.countryName.push(rowArray[3]);
+          dataBundle.lastUpdate.push(rowArray[4]);
+          dataBundle.lat.push(rowArray[5]);
+          dataBundle.lng.push(rowArray[6]);
+          dataBundle.Confirmed.push(rowArray[7]);
+          dataBundle.Deaths.push(rowArray[8]);
+          dataBundle.Recovered.push(rowArray[9]);
+          dataBundle.Active.push(rowArray[10]);
+
+          console.log(rowArray[5], rowArray[6]);
+          // console.log(dataBundle["lat"][obj.Countries[item].CountryCode]);
+          // console.log(dataBundle["lng"][obj.Countries[item].CountryCode]);
+  
+          var circleCenter = [rowArray[5], rowArray[6]];
+          var circleOptions = {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: .4,
+            weight: .5
+          }
+
+          let circleSize = rowArray[7]/20;
+          var circle = L.circle(circleCenter, circleSize, circleOptions);
+
+          if(rowArray[2] == ""){
+            circle.bindPopup("<b>" + rowArray[3] + "</b>");
+          }else{
+            circle.bindPopup("<b>" + rowArray[2] + "</b> - " + rowArray[3]);
+          }
+
+          
+          circle.addTo(map);
+        }
+        
+
+
+      }
+              
+      count += 1;
+    }
+    
+  };
+  
+  return dataBundle;
+}
 
 function loadWorldSummary(dataBundle){
 
@@ -228,9 +457,15 @@ function loadWorldSummary(dataBundle){
 
   Http_2.onloadend = (e) =>{
 
+    
+
     const obj = JSON.parse(Http_2.responseText);
     let minCases = 2^53 - 1;
     let maxCases = 0;
+
+    if(obj.Message == "Caching in progress"){
+      box.show( 'Servers are busy. Try again' );
+    }
 
     for (let item in obj.Countries){
       if(obj.Countries[item].TotalConfirmed < minCases){
@@ -252,9 +487,9 @@ function loadWorldSummary(dataBundle){
       let lng = dataBundle["lng"][obj.Countries[item].CountryCode];
 
       if (lat != undefined){
-        // console.log(obj.Countries[item].Country);
-        // console.log(dataBundle["lat"][obj.Countries[item].CountryCode]);
-        // console.log(dataBundle["lng"][obj.Countries[item].CountryCode]);
+        console.log(obj.Countries[item].Country);
+        console.log(dataBundle["lat"][obj.Countries[item].CountryCode]);
+        console.log(dataBundle["lng"][obj.Countries[item].CountryCode]);
 
 
         var circleCenter = [lat, lng];
@@ -308,15 +543,49 @@ L.control.zoom({position: 'topleft'}).addTo(map);
 
 
 // Zoom level change listener
-map.on("zoomstart", function (e) { console.log("ZOOMSTART", e); });
+let zoomConfig= {
+  previous:0,
+  current:0
+}
+map.on("zoomstart", function (e) { 
+  // console.log("ZOOMSTART", e);
+  // console.log("ZOOMEND", e.target._zoom); 
+  // zoomConfig.previous = e.target._zoom;
+});
 map.on("zoomend", function (e) { 
-  console.log("ZOOMEND", e); 
-  console.log("ZOOMEND", e.type); 
-  console.log("ZOOMEND", e.target._zoom); 
+  
+  if(map.getCenter().lng > 79.5 & map.getCenter().lng < 82){
+    if(map.getCenter().lat > 6 & map.getCenter().lat < 10){
+      if(e.target._zoom >= 7){
+        alert("Now you can see SL data only");
+        loadSLCovid();
+      }
+      
+    }
+
+    console.log("ZOOMEND", e); 
+    console.log("ZOOMEND", e.type); 
+    zoomConfig.current = e.target._zoom;
+
+  }
+
+});
+map.on("moveend", function (e) {
+
+  if(map.getCenter().lng > 79.5 & map.getCenter().lng < 82){
+    if(map.getCenter().lat > 6 & map.getCenter().lat < 10){
+
+      if(e.target._zoom >= 7){
+        alert("Now you can see SL data only");
+        loadSLCovid();
+      }
+    }
+  }
 });
 
-
-
+messegeBoxFunctionality();
+var options = { timeout: 5000 }
+var box = L.control.messagebox(options).addTo(map);
 // let Countries = [];
 // let CountryCode = [];
 // let TotalConfirmed = [];
@@ -330,7 +599,7 @@ map.on("zoomend", function (e) {
 
 
 
-loadCVS();
+let bulk = worldDataSource2();
 
 
 
@@ -355,3 +624,8 @@ loadCVS();
 // }
 
 // map.on('click', onMapClick);
+
+
+
+
+
